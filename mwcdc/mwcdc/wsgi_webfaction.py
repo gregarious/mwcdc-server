9130230b9c2d46519dbd73f9375c3e4986a83488
Greg Nicholas <greg.nicholas@gmail.com>
2013-08-23 13:25:48 -0400

    update wsgi to use webfaction settings

--- mwcdc/mwcdc/wsgi_webfaction.py
+++ mwcdc/mwcdc/wsgi_webfaction.py
@@ -7,7 +7,7 @@ stick with apache SetEnv.
 
 import os
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mwcdc.settings.development'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mwcdc.settings.webfaction'
 os.environ['SECRET_KEY'] = 'f9g*qtz(tnvd+@cer7f+ks*rr^l#+!7@sb0207b69a009o64'
 os.environ['DATABASE_URL'] = 'postgres://gregarious_mwcdc:gregarious_mwcdc@localhost/gregarious_mwcdc'
 
