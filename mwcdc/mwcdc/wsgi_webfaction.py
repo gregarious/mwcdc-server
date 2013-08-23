3645ee709f9398e8a31bb7e61a72de72532dc177
Greg Nicholas <greg.nicholas@gmail.com>
2013-08-23 12:41:21 -0400

    move webfaction settings to wsgi file

--- mwcdc/mwcdc/wsgi_webfaction.py
+++ mwcdc/mwcdc/wsgi_webfaction.py
@@ -1,7 +1,16 @@
+'''
+Copy of wsgi.py with webfaction env variables fixed. Can't get them to
+stick with apache SetEnv.
+
+*DON'T* use this for a real deploy.
+'''
+
 import os
 
-# can't get apache SetEnv to stick. Manually set env here.
-#
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mwcdc.settings.development'
 os.environ['SECRET_KEY'] = 'f9g*qtz(tnvd+@cer7f+ks*rr^l#+!7@sb0207b69a009o64'
 os.environ['DATABASE_URL'] = 'postgres://gregarious_mwcdc:gregarious_mwcdc@localhost/gregarious_mwcdc'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
