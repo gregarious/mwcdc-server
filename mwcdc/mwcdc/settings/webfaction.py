23297376eb6af906ae19157c35173dd63b25d22e
Greg Nicholas <greg.nicholas@gmail.com>
2013-08-23 13:14:44 -0400

    more static config

--- mwcdc/mwcdc/settings/webfaction.py
+++ mwcdc/mwcdc/settings/webfaction.py
@@ -1,4 +1,7 @@
 from development import *
 
-STATIC_ROOT = '/home/gregarious/webapps/mwcdc_static/static'
-MEDIA_ROOT = '/home/gregarious/webapps/mwcdc_static/media'
+STATIC_ROOT = '/home/gregarious/webapps/mwcdc_static/static/'
+MEDIA_ROOT = '/home/gregarious/webapps/mwcdc_static/media/'
+
+STATIC_URL = '/assets/static/'
+MEDIA_URL = '/assets/media/'
