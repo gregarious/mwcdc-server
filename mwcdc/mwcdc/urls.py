46e253a8aecf8a9e0620fb34c1db5e3c412184a0
Greg Nicholas <greg.nicholas@gmail.com>
2013-11-02 18:54:47 -0400

    clear out dev notes from index.html

--- mwcdc/mwcdc/urls.py
+++ mwcdc/mwcdc/urls.py
@@ -10,14 +10,7 @@ from skyline.views import ViewpointViewSet
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-    # url(r'^mwcdc/', include('mwcdc.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
 
