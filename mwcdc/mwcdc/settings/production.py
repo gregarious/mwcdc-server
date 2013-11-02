c750def35ebbddae9efe38bfabf3ba1d4fab3edc
Greg Nicholas <greg.nicholas@gmail.com>
2013-11-02 18:50:25 -0400

    disable browsable api in production

--- mwcdc/mwcdc/settings/production.py
+++ mwcdc/mwcdc/settings/production.py
@@ -2,3 +2,10 @@ from mwcdc.settings.base import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+# disable the browsable API
+REST_FRAMEWORK = {
+	'DEFAULT_RENDERER_CLASSES': (
+		'rest_framework.renderers.JSONRenderer',
+	)
+}
