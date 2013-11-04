39bc819e2b223e76df7a174e541c25ba77d858b4
Greg Nicholas <greg.nicholas@gmail.com>
2013-11-04 12:16:10 -0500

    place ModelAdmin updates

--- mwcdc/places/admin.py
+++ mwcdc/places/admin.py
@@ -1,15 +1,15 @@
 from places.models import Place
 from django.contrib import admin
-from django import forms
-from django.contrib import messages
 
 class PlaceModelAdmin(admin.ModelAdmin):
-    def get_readonly_fields(self, request, obj=None):
-        if not request.user.is_superuser:
-            return ('name', 'address', 'image', 'description', 'hours',
-                'fb_id', 'twitter_id', 'zip_code', 'phone', 'contact_email',
-                'website', 'category')
-        else:
-            return tuple()
+	exclude = ('category_id', 'external_image_url',)
+
+	def get_readonly_fields(self, request, obj=None):
+		if not request.user.is_superuser:
+			return ('name', 'street_address', 'image', 'description', 'hours',
+				'fb_id', 'twitter_handle', 'zip_code', 'phone', 'contact_email',
+				'website', 'category_label')
+		else:
+			return tuple()
 
 admin.site.register(Place, PlaceModelAdmin)
