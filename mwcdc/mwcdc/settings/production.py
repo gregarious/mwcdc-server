6621a592245f1ca1831272b6b74052eeab34eeef
Greg Nicholas <greg.nicholas@gmail.com>
2013-11-04 10:23:48 -0500

    add static file support for production server

--- mwcdc/mwcdc/settings/production.py
+++ mwcdc/mwcdc/settings/production.py
@@ -9,3 +9,21 @@ REST_FRAMEWORK = {
 		'rest_framework.renderers.JSONRenderer',
 	)
 }
+
+STATIC_ROOT = os.path.join(
+				os.path.dirname(	# mwcdc (Webfaction webapp root)
+					os.path.dirname(	# mwcdc-server (repo)
+						os.path.dirname(	# mwcdc (project)
+							os.path.dirname(	# mwcdc (app)
+								os.path.dirname(	# settings
+									os.path.abspath(__file__)))))),
+				'mwcdc-server_assets/static/')
+
+MEDIA_ROOT = os.path.join(
+				os.path.dirname(	# mwcdc (Webfaction webapp root)
+					os.path.dirname(	# mwcdc-server (repo)
+						os.path.dirname(	# mwcdc (project)
+							os.path.dirname(	# mwcdc (app)
+								os.path.dirname(	# settings
+									os.path.abspath(__file__)))))),
+				'mwcdc-server_assets/media/')
