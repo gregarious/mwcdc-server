ed00abcab33ae98e801cf058eab094e924f0f9fa
Greg Nicholas <greg.nicholas@gmail.com>
2014-01-10 16:56:18 -0500

    add initial dotcloud deployment files

---
+++ mwcdc/mwcdc/settings/dotcloud.py
@@ -0,0 +1,7 @@
+from base import *
+
+STATIC_ROOT = '/home/dotcloud/volatile/static/'
+MEDIA_ROOT = '/home/dotcloud/data/media/'
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
---
+++ mwcdc/wsgi.py
@@ -0,0 +1,29 @@
+'''
+wsgi.py settings for Dotcloud
+'''
+
+import os
+import json
+import sys
+
+# set the $DATABASE_URL from the dotcloud environment file
+with open('/home/dotcloud/environment.json') as f:
+	env = json.load(f)
+
+os.environ['DATABASE_URL'] = 'postgres://%(user)s:%(password)s@%(host)s:%(port)s/%(name)s' % {
+	'name': 'mwcdcserverapp_production',
+	'user': env['DOTCLOUD_DB_SQL_LOGIN'],
+	'password': env['DOTCLOUD_DB_SQL_PASSWORD'],
+	'host': env['DOTCLOUD_DB_SQL_HOST'],
+	'port': env['DOTCLOUD_DB_SQL_PORT'],
+}
+
+os.environ['SECRET_KEY'] = env['SECRET_KEY']
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mwcdc.settings.dotcloud'
+
+# add primary PYTHONPATH dir to path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'mwcdc')))
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
