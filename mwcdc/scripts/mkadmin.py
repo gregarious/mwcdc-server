fc8c31d944c45e853836952982f82e04dcf82abf
Greg Nicholas <greg.nicholas@gmail.com>
2014-01-13 16:56:24 -0500

    add extra warning about default password

--- mwcdc/scripts/mkadmin.py
+++ mwcdc/scripts/mkadmin.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 from wsgi import *
 from django.contrib.auth.models import User
-u, created = User.objects.get_or_create(username='admin')
+from sys import stderr as err
+from django.contrib.auth import authenticate
+
+DEFAULT_USERNAME = 'admin'
+DEFAULT_PASSWORD = 'password'
+
+u, created = User.objects.get_or_create(username=DEFAULT_USERNAME)
 if created:
-    u.set_password('password')
+    u.set_password(DEFAULT_PASSWORD)
     u.is_superuser = True
     u.is_staff = True
     u.save()
-    print 'Administrator account created. Change password now!'
+	
+    err.write('Administrator account created with default password. Change it now!\n')
+    err.flush()
+else:
+	if authenticate(username=DEFAULT_USERNAME, password=DEFAULT_PASSWORD):
+		err.write('Administrator account still has default password. Change it now!\n')
+		err.flush()
