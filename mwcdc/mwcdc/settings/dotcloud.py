11a21093f4aeefd1f644d1f1d7ab7b487eeef4fb
Greg Nicholas <greg.nicholas@gmail.com>
2014-01-19 22:17:33 -0500

    add settings changes

--- mwcdc/mwcdc/settings/dotcloud.py
+++ mwcdc/mwcdc/settings/dotcloud.py
@@ -1,7 +1,19 @@
 from base import *
 
+DEBUG = False
+TEMPLATE_DEBUG = False
+
 STATIC_ROOT = '/home/dotcloud/volatile/static/'
 MEDIA_ROOT = '/home/dotcloud/data/media/'
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+
+# disable the browsable API
+REST_FRAMEWORK = {
+	'DEFAULT_RENDERER_CLASSES': (
+		'rest_framework.renderers.JSONRenderer',
+	)
+}
+
+ALLOWED_HOSTS = ['mwcdcappserver-mountwashington.dotcloud.com']
